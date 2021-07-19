90c1c43b85895a2239c5044c855e78b1e45e99d7
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-07-19 15:52:48 +0800

    lay down start to tab switching

--- src/index.js
+++ src/index.js
@@ -3,9 +3,57 @@ import './style.css';
 
 const mainEl = document.querySelector('main');
 
-function initialize()
+let currPage = 'home';
+
+function setHomePage()
 {
 	mainEl.append(...homePage());
+}
+
+function setEventListeners()
+{
+	const headerNav = document.querySelector('#headerNav');
+	const navBtns = headerNav.querySelectorAll('button');
+
+	function callback(e)
+	{
+		const tabName = e.target.textContent.toLowerCase();
+
+		if(currPage !== tabName)
+		{
+			currPage = tabName;
+			removeChildren(mainEl);
+
+			let newMainChildren;
+			switch(tabName)
+			{
+				case 'home':
+				default:
+					newMainChildren = homePage();
+				 	break;
+			}
+
+			mainEl.append(...newMainChildren)
+		}
+	}
+
+	for(const navBtn of navBtns)
+	{
+		navBtn.addEventListener('click', callback);
+	}
+}
+
+function removeChildren(node)
+{
+	while (node.firstChild) {
+		node.removeChild(node.lastChild);
+	}
+}
+
+function initialize()
+{
+	setHomePage();
+	setEventListeners();
 }
 
 initialize();
